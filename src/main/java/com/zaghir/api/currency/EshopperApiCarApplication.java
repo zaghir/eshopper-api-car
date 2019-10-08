1c67b3e1ed6650731cdcc1bffcbf94eb5ae178fb
zaghir <zaghir.youssef@gmail.com>
2019-10-08 14:29:13 +0200

    first commit

---
+++ src/main/java/com/zaghir/api/currency/EshopperApiCarApplication.java
@@ -0,0 +1,13 @@
+package com.zaghir.api.currency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshopperApiCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EshopperApiCarApplication.class, args);
+	}
+
+}
